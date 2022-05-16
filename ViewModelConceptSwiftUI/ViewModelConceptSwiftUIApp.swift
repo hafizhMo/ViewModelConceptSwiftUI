63d9b6035ac608ab9b9cb4ee305b50ec9f29560f
hafizhMo <hafizh.hizb@gmail.com>
2022-05-16 11:25:52 +0700

    Initial Commit

---
+++ ViewModelConceptSwiftUI/ViewModelConceptSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelConceptSwiftUIApp.swift
+//  ViewModelConceptSwiftUI
+//
+//  Created by Hafizh Mo on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewModelConceptSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
